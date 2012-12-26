6a791c03f3b1be12b947e618f34f1bf663402f26
Josh <joshuajnoble@gmail.com>
2012-12-25 22:57:16 -0800

    just a quick backup, wip, but worried about losing files again.

--- samples/Skeleton/src/OpenNIApp.cpp
+++ samples/Skeleton/src/OpenNIApp.cpp
@@ -1,14 +1,13 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
 
-#include "OpenNI.h"
+#define USING_NITE
+#include <ciOpenNI.h>
 
 using namespace ci;
-using namespace ci::app;
 using namespace std;
-
-#define SAMPLE_XML_PATH "ONIConfig.xml"
-#define MAX_NUM_USERS 2
+using namespace ci::app;
 
 class OpenNIApp : public AppBasic {
   public:
@@ -17,137 +16,52 @@ class OpenNIApp : public AppBasic {
 	void update();
 	void draw();
     
-    XnBool mNeedPose;
-    string strPose;
-    
-    XnCallbackHandle hUserCallbacks, hCalibrationStart, hCalibrationComplete, hPoseDetected, hCalibrationInProgress, hPoseInProgress;
-    
-    xn::Context mContext;
-    xn::ScriptNode mScriptNode;
-    xn::DepthGenerator mDepth;
-    xn::ImageGenerator mImage;
-    xn::UserGenerator mUserGenerator;
-    
-    xn::DepthMetaData mdepthMD;
-    xn::ImageMetaData mimageMD;
-    
-    gl::Texture color, depth;
-    vector< vector<Vec3f> > userJoints;
-    
-    
     bool calibratedUser;
     
-    // OpenNI callbacks
-    // ---------------------------------------------------------------------------------
-    
-    // Callback: New user was detected
-    static void XN_CALLBACK_TYPE User_NewUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie) {
-        OpenNIApp *app = static_cast<OpenNIApp*>(pCookie);
-        app->newUser(generator, nId);
-    }
-    
-    // Callback: An existing user was lost
-    static void XN_CALLBACK_TYPE User_LostUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie) {
-        OpenNIApp *app = static_cast<OpenNIApp*>(pCookie);
-        app->lostUser(generator, nId);
-    }
-    
-    // Callback: Detected a pose
-    static void XN_CALLBACK_TYPE UserPose_PoseDetected(xn::PoseDetectionCapability& capability, const XnChar* strPose, XnUserID nId, void* pCookie) {
-        OpenNIApp *app = static_cast<OpenNIApp*>(pCookie);
-        app->poseDetected(capability, strPose, nId);
-    }
-    
-    // Callback: Started calibration
-    static void XN_CALLBACK_TYPE UserCalibration_CalibrationStart(xn::SkeletonCapability& capability, XnUserID nId, void* pCookie) {
-        OpenNIApp *app = static_cast<OpenNIApp*>(pCookie);
-        app->calibrationStart(capability, nId);
-    }
-    
-    // Callback: Calibration Complete
-    static void XN_CALLBACK_TYPE UserCalibration_CalibrationComplete(xn::SkeletonCapability& capability, XnUserID nId, XnCalibrationStatus eStatus, void* pCookie) {
-        
-        OpenNIApp *app = static_cast<OpenNIApp*>(pCookie);
-        app->calibrationComplete(capability, nId, eStatus);
-    }
+	openni::Device mDevice;
+	nite::UserTracker* mTracker;
 
-    void newUser(xn::UserGenerator& generator, XnUserID nId);
-    void lostUser(xn::UserGenerator& generator, XnUserID nId);
-    void poseDetected(xn::PoseDetectionCapability& capability, const XnChar* strPose, XnUserID nId);
-    void calibrationStart(xn::SkeletonCapability& capability, XnUserID nId);
-    void calibrationComplete(xn::SkeletonCapability& capability, XnUserID nId, XnCalibrationStatus eStatus);
-    
+	bool mVisibleUsers[10];
+	nite::SkeletonState mSkeletonStates[10];
+
+	gl::Texture colorTex, depthTex;
+
 };
 
 void OpenNIApp::setup()
 {
     
-    calibratedUser = false;
-    strPose = "";
-    
-    string str = getResourcePath() + "/ONIConfig.xml";
-    
-    xn::EnumerationErrors errors;
-    
-    XnStatus ret = XN_STATUS_OK;
-    
-    ret = mContext.InitFromXmlFile(str.c_str(), mScriptNode, &errors);
-   
-    if (ret != XN_STATUS_OK) { console() << " can't make context "; }
-    
-    ret = mContext.FindExistingNode(XN_NODE_TYPE_DEPTH, mDepth);
-    
-    if (ret != XN_STATUS_OK) { console() << " can't make depth "; }
-    
-    ret = mContext.FindExistingNode(XN_NODE_TYPE_IMAGE, mImage);
-    
-    if (ret != XN_STATUS_OK) { console() << " can't make image "; }
-    
-    ret = mUserGenerator.Create(mContext);
-        
-    if (ret != XN_STATUS_OK) { console() << " can't create generator "; }
-    
-    if (!mUserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON)) {
-        console() << "Supplied user generator doesn't support skeleton ";
-        return;
-    } else {
-        console() << " let's do skeletons ";
-    }
-    
-    if (!mUserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION)) {
-        console() << " can do poses ";
-        return;
-    }
+	for( int i = 0; i < 10; i++) { mSkeletonStates[i] = {nite::SKELETON_NONE}; }
 
-    if (mUserGenerator.GetSkeletonCap().NeedPoseForCalibration())
-    {
-        XnChar requiredPose[20] = "";
-        
-        if (!mUserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION))
-        {
-            console() << "Pose required, but not supported ";
-            return;
-        }
-        
-        mNeedPose = true;
-        
-        ret = mUserGenerator.GetPoseDetectionCap().RegisterToPoseDetected(UserPose_PoseDetected, NULL, hPoseDetected);
-        if (ret != XN_STATUS_OK) { console() << " gotta get a pose "; }
-        mUserGenerator.GetSkeletonCap().GetCalibrationPose(requiredPose);
-        console() << " required pose is " << requiredPose;
-    }
-    
-    ret = mContext.StartGeneratingAll();
-    if (ret != XN_STATUS_OK) { console() << " can't start generating "; }
-    
-    mUserGenerator.RegisterUserCallbacks(&OpenNIApp::User_NewUser, &OpenNIApp::User_LostUser, this, hUserCallbacks);
-    mUserGenerator.GetSkeletonCap().RegisterToCalibrationStart(&OpenNIApp::UserCalibration_CalibrationStart, this, hCalibrationStart);
-    mUserGenerator.GetSkeletonCap().RegisterToCalibrationComplete(&OpenNIApp::UserCalibration_CalibrationComplete, this, hCalibrationComplete);
-    
+	openni::Status rc = openni::OpenNI::initialize();
+	if (rc != openni::STATUS_OK)
+	{
+		console() << "Failed to initialize OpenNI " << openni::OpenNI::getExtendedError() << endl;
+		return;
+	}
+
+	const char* deviceUri = openni::ANY_DEVICE;
+	rc = mDevice.open(deviceUri);
+
+	if (rc != openni::STATUS_OK)
+	{
+		console() << "Failed to open device ", openni::OpenNI::getExtendedError());
+		return;
+	}
+
+	nite::NiTE::initialize();
+
+	if (mTracker->create(&mDevice) != nite::STATUS_OK)
+	{
+		console() << " Status error " <<  openni::STATUS_ERROR << endl;
+		return;
+	}
+
+
     gl::Texture::Format format;
     
-    color = gl::Texture(640, 480, format);
-    depth = gl::Texture(320, 240, format);
+    colorTex = gl::Texture(640, 480, format);
+    depthTex = gl::Texture(320, 240, format);
     
 }
 
@@ -158,64 +72,164 @@ void OpenNIApp::mouseDown( MouseEvent event )
 void OpenNIApp::update()
 {
     
-    XnStatus ret = XN_STATUS_OK;
-    ret = mContext.WaitAnyUpdateAll();
-	if (ret != XN_STATUS_OK)
+	nite::UserTrackerFrameRef userTrackerFrame;
+	openni::VideoFrameRef depthFrame;
+	nite::Status rc = mTracker->readFrame(&userTrackerFrame);
+	if (rc != nite::STATUS_OK)
 	{
-        console() << " can't update ";
-        return;
-    }
+		console() << "Get Data from user tracker failed " << endl;
+		return;
+	}
+
+	depthFrame = userTrackerFrame.getDepthFrame();
+
+    const nite::Array<nite::UserData>& users = userTrackerFrame.getUsers();
+	for (int i = 0; i < users.getSize(); ++i)
+	{
+		const nite::UserData& user = users[i];
+
+		//updateUserState(user, userTrackerFrame.getTimestamp());
+
+	if (user.isNew()) {
+		console() << "New");
+	} else if (user.isVisible() && !mVisibleUsers[user.getId()]) {
+		console() << "User " << user.getId() << " visible at " << userTrackerFrame.getTimestamp() << endl;
+	} else if (!user.isVisible() && mVisibleUsers[user.getId()]) {
+		console() << "User " << user.getId() << " visible at " << userTrackerFrame.getTimestamp() << endl;
+	} else if (user.isLost()) {
+		console() << "Lost");
+	}
+	mVisibleUsers[user.getId()] = user.isVisible();
+
+
+	if(mSkeletonStates[user.getId()] != user.getSkeleton().getState())
+	{
+		switch(mSkeletonStates[user.getId()] = user.getSkeleton().getState())
+		{
+		case nite::SKELETON_NONE:
+			console() << "Stopped tracking.")
+			break;
+		case nite::SKELETON_CALIBRATING:
+			console() << "Calibrating...")
+			break;
+		case nite::SKELETON_TRACKED:
+			console() << "Tracking!")
+			break;
+		case nite::SKELETON_CALIBRATION_ERROR_NOT_IN_POSE:
+		case nite::SKELETON_CALIBRATION_ERROR_HANDS:
+		case nite::SKELETON_CALIBRATION_ERROR_LEGS:
+		case nite::SKELETON_CALIBRATION_ERROR_HEAD:
+		case nite::SKELETON_CALIBRATION_ERROR_TORSO:
+			console() << "Calibration Failed... :-|" << endl;
+			break;
+		}
+	}
+
+		if (user.isNew())
+		{
+			mTracker->startSkeletonTracking(user.getId());
+			mTracker->startPoseDetection(user.getId(), nite::POSE_CROSSED_HANDS);
+		}
+		else if (!user.isLost())
+		{
+			if (users[i].getSkeleton().getState() == nite::SKELETON_TRACKED)
+			{
+				DrawSkeleton(mTracker, user);
+			}
+		}
+
+		if (m_poseUser == 0 || m_poseUser == user.getId())
+		{
+			const nite::PoseData& pose = user.getPose(nite::POSE_CROSSED_HANDS);
+
+			if (pose.isEntered())
+			{
+				// Start timer
+				sprintf(g_generalMessage, "In exit pose. Keep it for %d second%s to exit\n", g_poseTimeoutToExit/1000, g_poseTimeoutToExit/1000 == 1 ? "" : "s");
+				printf("Counting down %d second to exit\n", g_poseTimeoutToExit/1000);
+				m_poseUser = user.getId();
+				m_poseTime = userTrackerFrame.getTimestamp();
+			}
+			else if (pose.isExited())
+			{
+				memset(g_generalMessage, 0, sizeof(g_generalMessage));
+				printf("Count-down interrupted\n");
+				m_poseTime = 0;
+				m_poseUser = 0;
+			}
+			else if (pose.isHeld())
+			{
+				// tick
+				if (userTrackerFrame.getTimestamp() - m_poseTime > g_poseTimeoutToExit * 1000)
+				{
+					printf("Count down complete. Exit...\n");
+					Finalize();
+					exit(2);
+				}
+			}
+		}
+	}
+
     
-    mDepth.GetMetaData(mdepthMD);
-	mImage.GetMetaData(mimageMD);
-    
-    depth = Surface( fromOpenNI(mdepthMD) );
-    color = Surface( fromOpenNI(mimageMD));
-    
-    XnUserID aUsers[MAX_NUM_USERS];
-    XnUInt16 nUsers;
-    XnSkeletonJointTransformation torsoJoint;
-    
-    // print the torso information for the first user already tracking
-    mUserGenerator.GetUsers(aUsers, nUsers);
-    int numTracked=0;
-    int userToPrint=-1;
-    for(XnUInt16 i=0; i<nUsers; i++)
-    {
-        if(mUserGenerator.GetSkeletonCap().IsTracking(aUsers[i])==FALSE)
-            continue;
-        
-        // make 1 user
-        userJoints.push_back( vector<Vec3f>() );
-        
-        // get ready to get all joints
-        XnSkeletonJoint pJoints[24]; // there can't be more than 24
-        XnUInt16 numberOfJoints;
-        
-        // find out how many there are
-        mUserGenerator.GetSkeletonCap().EnumerateActiveJoints(&pJoints[0], numberOfJoints);
-        
-        
-        XnSkeletonJointPosition *trans = new XnSkeletonJointPosition[numberOfJoints];
-        XnPoint3D *realWorldPoint = new XnPoint3D[numberOfJoints];
-        
-        // now loop through each joint and get its position
-        int j = 0;
-        while( j < numberOfJoints) {
-            mUserGenerator.GetSkeletonCap().GetSkeletonJointPosition(aUsers[i], pJoints[j], trans[j]);
-            mDepth.ConvertRealWorldToProjective(1, &trans[i].position, &realWorldPoint[i]);
-            userJoints.back().push_back( fromOpenNI(realWorldPoint[i]));
-            j++;
-        }
-        
-        // just get one
-        //XnSkeletonJointTransformation torsoJoint;
-        //mUserGenerator.GetSkeletonCap().GetSkeletonJoint(aUsers[i],XN_SKEL_TORSO,torsoJoint);
-        //mDepth.ConvertRealWorldToProjective(1, &torsoJoint.position.position, &realWorldPoint);
-        //console() << "user " << aUsers[i] << " head at " << torsoJoint.position.position.X << "  " << torsoJoint.position.position.Y << " " << torsoJoint.position.position.Z;
-    }
-    
-    
+}
+
+void OpenNIApp::DrawSkeleton(nite::UserTracker* pUserTracker, const nite::UserData& userData)
+{
+
+	float coordinates[6] = {0};
+	pUserTracker->convertJointCoordinatesToDepth(joint1.getPosition().x, joint1.getPosition().y, joint1.getPosition().z, &coordinates[0], &coordinates[1]);
+	pUserTracker->convertJointCoordinatesToDepth(joint2.getPosition().x, joint2.getPosition().y, joint2.getPosition().z, &coordinates[3], &coordinates[4]);
+
+	coordinates[0] *= GL_WIN_SIZE_X/g_nXRes;
+	coordinates[1] *= GL_WIN_SIZE_Y/g_nYRes;
+	coordinates[3] *= GL_WIN_SIZE_X/g_nXRes;
+	coordinates[4] *= GL_WIN_SIZE_Y/g_nYRes;
+
+	if (joint1.getPositionConfidence() == 1 && joint2.getPositionConfidence() == 1)
+	{
+		glColor3f(1.0f - Colors[color][0], 1.0f - Colors[color][1], 1.0f - Colors[color][2]);
+	}
+	else if (joint1.getPositionConfidence() < 0.5f || joint2.getPositionConfidence() < 0.5f)
+	{
+		return;
+	}
+	else
+	{
+		glColor3f(.5, .5, .5);
+	}
+
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_HEAD), userData.getSkeleton().getJoint(nite::JOINT_NECK), userData.getId() % colorCount);
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_LEFT_SHOULDER), userData.getSkeleton().getJoint(nite::JOINT_LEFT_ELBOW), userData.getId() % colorCount);
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_LEFT_ELBOW), userData.getSkeleton().getJoint(nite::JOINT_LEFT_HAND), userData.getId() % colorCount);
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_RIGHT_SHOULDER), userData.getSkeleton().getJoint(nite::JOINT_RIGHT_ELBOW), userData.getId() % colorCount);
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_RIGHT_ELBOW), userData.getSkeleton().getJoint(nite::JOINT_RIGHT_HAND), userData.getId() % colorCount);
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_LEFT_SHOULDER), userData.getSkeleton().getJoint(nite::JOINT_RIGHT_SHOULDER), userData.getId() % colorCount);
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_LEFT_SHOULDER), userData.getSkeleton().getJoint(nite::JOINT_TORSO), userData.getId() % colorCount);
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_RIGHT_SHOULDER), userData.getSkeleton().getJoint(nite::JOINT_TORSO), userData.getId() % colorCount);
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_TORSO), userData.getSkeleton().getJoint(nite::JOINT_LEFT_HIP), userData.getId() % colorCount);
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_TORSO), userData.getSkeleton().getJoint(nite::JOINT_RIGHT_HIP), userData.getId() % colorCount);
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_LEFT_HIP), userData.getSkeleton().getJoint(nite::JOINT_RIGHT_HIP), userData.getId() % colorCount);
+
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_LEFT_HIP), userData.getSkeleton().getJoint(nite::JOINT_LEFT_KNEE), userData.getId() % colorCount);
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_LEFT_KNEE), userData.getSkeleton().getJoint(nite::JOINT_LEFT_FOOT), userData.getId() % colorCount);
+
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_RIGHT_HIP), userData.getSkeleton().getJoint(nite::JOINT_RIGHT_KNEE), userData.getId() % colorCount);
+	DrawLimb(pUserTracker, userData.getSkeleton().getJoint(nite::JOINT_RIGHT_KNEE), userData.getSkeleton().getJoint(nite::JOINT_RIGHT_FOOT), userData.getId() % colorCount);
+}
+
+void OpenNIApp::exit()
+{
+	delete mTracker;
+	nite::NiTE::shutdown();
+	openni::OpenNI::shutdown();
 }
 
 void OpenNIApp::draw()
@@ -225,81 +239,6 @@ void OpenNIApp::draw()
     
     //gl::draw( depth );
     gl::draw( color );
-}
-
-
-////////////////////////////////////////////
-/* 
- OpenNI callbacks
- You need to add these to your application to have it receive events from OpenNI
- I thought about making these something hidden that your application would tie into
- but on reflection, thought that was not entirely in the spirit of Cinder and that
- most people would want to do something of sufficient complexity that they would need
- to have these in their own application
-*/
-////////////////////////////////////////////
-
-// local callbacks
-
-void OpenNIApp::newUser(xn::UserGenerator& generator, XnUserID nId) {
-    
-    console() << " new user " << nId ;
-    
-    // New user found
-    if (mNeedPose)
-    {
-        mUserGenerator.GetPoseDetectionCap().StartPoseDetection(strPose.c_str(), nId);
-    }
-    else
-    {
-        mUserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
-    }
-
-}
-
-void OpenNIApp::lostUser(xn::UserGenerator& generator, XnUserID nId) {
-    console() << " Lost user " << nId;	
-}
-
-void OpenNIApp::poseDetected(xn::PoseDetectionCapability& capability, const XnChar* strPose, XnUserID nId) {
-    
-    console() << " pose detected " << nId ;
-    
-    mUserGenerator.GetPoseDetectionCap().StopPoseDetection(nId);
-    mUserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
-
-}
-
-void OpenNIApp::calibrationStart(xn::SkeletonCapability& capability, XnUserID nId) {
-    console() << " Calibration started for user " << nId ;
-}
-
-void OpenNIApp::calibrationComplete(xn::SkeletonCapability& capability, XnUserID nId, XnCalibrationStatus eStatus) {
-    
-    if (eStatus == XN_CALIBRATION_STATUS_OK)
-    {
-        // Calibration succeeded
-        console() << " Calibration complete, start tracking user " << nId;
-        mUserGenerator.GetSkeletonCap().StartTracking(nId);
-    }
-    else
-    {
-        // Calibration failed
-        //console() << " Calibration failed for user " << nId;
-        if(eStatus==XN_CALIBRATION_STATUS_MANUAL_ABORT)
-        {
-            console() << "Manual abort occured, stop attempting to calibrate!";
-            return;
-        }
-        if (mNeedPose)
-        {
-            mUserGenerator.GetPoseDetectionCap().StartPoseDetection(strPose.c_str(), nId);
-        }
-        else
-        {
-            mUserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
-        }
-    }
 }
 
 
